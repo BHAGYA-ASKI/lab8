361efc0fa2646eb7ecdcf31ff1ef1809944675a2
BHAGYA-ASKI <163536519+BHAGYA-ASKI@users.noreply.github.com>
2025-03-20 23:27:43 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Jenkins" << endl;
+    cout << "This is Bhagya Budharaju: PES2UG22CS127 - LAB 08" << endl;
+    return 0;
+}
+
